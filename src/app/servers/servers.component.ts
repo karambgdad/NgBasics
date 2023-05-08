611db23952bac6a66c3b9d34faad0480077fa934
karam Baghdad <karambdad@gmail.com>
2023-05-08 03:38:43 +0200

    v3

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -9,11 +9,25 @@ import { Component } from '@angular/core';
 })
 export class ServersComponent {
   allowNewServer = false;
+  serverCreationStatus = 'No server was created'
+  serverName = '';
 
-  constructor(){
-    setTimeout( ()=>{
-      this.allowNewServer=true;
-    } ,2000)
+  constructor() {
+    setTimeout(() => {
+      this.allowNewServer = true;
+    }, 2000)
   }
+  onCreateServer() {
+    this.serverCreationStatus = 'server was created'
+  }
+  onUpdaterServerName(event : Event){
+     this.serverName = (<HTMLInputElement>event.target).value;
+// this is just needed to inform type script
+// that we know that the type of the HTML element
+// of this event will be an HTML input element.
+// We do this explicit casting with this syntax
+// with the opening and closing tag with the smaller
+// and greater than sign and to type in between.
 
+  }
 }
